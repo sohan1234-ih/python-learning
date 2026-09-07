54f36be5c379eea89c4cc162894c17a26db3cb2c
C Sohan <sohanprabhath2009@gmail.com>
2026-09-07 14:34:16 +0530

    working on json finished

--- proj11.py
+++ proj11.py
@@ -13,3 +13,8 @@ print(data)
 result=json.loads(data)
 print(result)
 print(result["name"])
+with open("student.json","w") as f:
+    json.dump(student,f)
+with open("student.json","r") as f:
+    test=json.load(f)
+print(test)
