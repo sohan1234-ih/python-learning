549ffcf0798bf111be3cbafa2a33bd4288c8dff6
C Sohan <sohanprabhath2009@gmail.com>
2026-09-12 22:39:16 +0530

    working on csv files

--- proj5.py
+++ proj5.py
@@ -3,9 +3,9 @@ student2=set()
 n=int(input("Enter the number of subjects taken by student 1:"))
 m=int(input("Enter the number of subjects taken by student 2:"))
 for i in range(0,n):
-    student1.add(input("enter the subjects").lower())
+    student1.add(input("enter the subjects of student 1").lower())
 for i in range(0,m):
-    student2.add(input("enter the subjects").lower())
+    student2.add(input("enter the subjects of student 2").lower())
 
 print(f"The total subjects are:{student1|student2}")
 print(f"The common subjects are:{student1&student2}")
